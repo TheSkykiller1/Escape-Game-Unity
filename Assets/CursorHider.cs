f80650234739764165eac613782b5b7f79d2cb0a
TheSkykiller1 <acqua.rium@hotmail.fr>
2018-05-31 16:50:50 +0200

    Add chest doors interaction
    An update

---
+++ Assets/CursorHider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
+        Cursor.lockState = CursorLockMode.Locked;   // keep confined to center of screen
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
+        Cursor.lockState = CursorLockMode.Locked;   // keep confined to center of screen
+	}
+}
