786ab9ca3f5bfd5590a5b1397a4e0553908abd6b
TheSkykiller1 <acqua.rium@hotmail.fr>
2018-06-07 18:13:33 +0200

    Update
    An update

---
+++ Assets/Scripts/triggered.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggered : MonoBehaviour {
+	
+    public bool openinggrille = false;
+    public Transform Steel_Window;
+
+    void Update()
+    {
+        if (openinggrille) Opengrille();
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.transform.name == "tournevis")
+        {
+            openinggrille = true;
+        }
+    }
+
+    void Opengrille()
+    {
+        float movement = 3.0f;
+        Steel_Window.position = new Vector3(
+            Steel_Window.position.x,
+            Steel_Window.position.y,
+            Steel_Window.position.z + movement
+        );
+    }
+}
