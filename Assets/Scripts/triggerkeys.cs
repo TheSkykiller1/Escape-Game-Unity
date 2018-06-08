e70551e22ec71bf1a3c6dfdc0d29e98b50a5a76e
TheSkykiller1 <acqua.rium@hotmail.fr>
2018-06-08 10:52:25 +0200

    Update traverse
    An update

---
+++ Assets/Scripts/triggerkeys.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTK.Examples
+{
+	public class triggerkeys : MonoBehaviour {
+	public Transform coffretournevis;
+	private Rigidbody Door;
+	void OnTriggerEnter(Collider obj){
+		if (obj.transform.name == "keys_locks"){
+			OpencoffreTel();
+		}
+	}
+
+	void OpencoffreTel(){
+		Door= coffretournevis.GetComponent<Rigidbody>();
+		Door.freezeRotation = false;
+	}
+	}
+}
